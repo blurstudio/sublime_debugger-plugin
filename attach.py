f080444bceb331d3719becb70cd7418438f83061
Philippe Cutillas <pcutillas@email.arizona.edu>
2021-01-18 16:02:47 -0800

    updated template to no longer need changes to debugger repo

--- attach.py
+++ attach.py
@@ -6,11 +6,12 @@ This script adds the the containing package as a valid debug adapter in the Debu
 """
 
 from os.path import join, abspath, dirname
+from threading import Timer
 import sublime
-import json
+import time
 
 
-adapter_type = "template"  # NOTE: type must be unique to each adapter
+adapter_type = "template"  # NOTE: type name must be unique to each adapter
 package_path = dirname(abspath(__file__))
 adapter_path = join(package_path, "adapter")  # customize this to work with your DAP implementation
 json_path = join(package_path, "sublime_debugger.json")
@@ -26,40 +27,61 @@ config_snippets = [
         "label": "Template Plugin",
         "description": "A template for creating DAP implementations for Sublime Text 3",
         "body": {
+            # These keys are required in each configuration
             "name": "Template Plugin",  
             "type": adapter_type,
-            "program": "\${file\}",
             "request": "attach",  # can only be attach or launch
-            "extra setting": "\${lookbelow\}"
+
+            # Python adapters usually require this one
+            "program": "\${file\}",
+
+            # The rest of the settings are up to you. These (and the above) can
+            # be modified by the user and will be sent to your adapter at runtime
+            "custom setting 1": "",
+            "custom setting 2": 0,
+            "custom setting group": {
+                "setting 1": True,
+            }
         }
     },
 ]
 
 # The settings used by the Debugger to run the adapter.
-# Variables under "settings" will be used to fill out configurations at runtime
 settings = {
     "type": adapter_type,
     "command": ["python", adapter_path],
     "install info": json_path,
-    "settings": {
-        # These must all be strings.
-        # Convert them back to bools/ints in your adapter when recieved
-        "lookbelow": "User can change this in Debugger settings, "
-                     "and it gets filled into the configs above at runtime"
-    }
 }
 
-# Write contents to json immediately
-with open(json_path, 'w') as f:
-    json.dump({
-        "version": version,
-        "configurationSnippets": config_snippets
-    }, f)
+# Instantiate variables needed for checking thread
+running = False
+check_speed = 1  # number of seconds to wait between checks for adapter presence in debugger instances
+
+
+def check_for_adapter():
+    """
+    Gets run in a thread to inject configuration snippets and version information 
+    into adapter objects of type adapter_type in each debugger instance found
+    """
+    
+    from Debugger.modules.debugger.debugger import Debugger
+
+    while running:
+
+        time.sleep(check_speed)
+
+        for instance in Debugger.instances.values():
+            adapter = getattr(instance, "adapters", {}).get(adapter_type, None)
+            
+            if adapter and not adapter.version:
+                adapter.version = version
+                adapter.snippets = config_snippets
 
 
 def plugin_loaded():
+    """ Add adapter to debugger settings for it to be recognized """
 
-    # Add adapter to debugger settings for it to be recognized
+    # Add entry to debugger settings
     debugger_settings = sublime.load_settings('debugger.sublime-settings')
     adapters_custom = debugger_settings.get('adapters_custom', {})
 
@@ -68,10 +90,21 @@ def plugin_loaded():
     debugger_settings.set('adapters_custom', adapters_custom)
     sublime.save_settings('debugger.sublime-settings')
 
+    # Start checking thread
+    global running, timer
+    running = True
+    Timer(1, check_for_adapter).start()
+
 
 def plugin_unloaded():
+    """ This is done every unload just in case this adapter is being uninstalled """
 
-    # Remove entry from debugger settings (in case this adapter is being uninstalled)
+    # Wait for checking thread to finish
+    global running
+    running = False
+    time.sleep(check_speed + .1)
+
+    # Remove entry from debugger settings
     debugger_settings = sublime.load_settings('debugger.sublime-settings')
     adapters_custom = debugger_settings.get('adapters_custom', {})
 
