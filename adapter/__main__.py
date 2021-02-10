711b428869eae50bebc3170e3c2c7d6e72087c33
Philippe Cutillas <pcutillas@email.arizona.edu>
2021-02-09 23:55:23 -0800

    updated to use a class-based stdio interface

--- adapter/__main__.py
+++ adapter/__main__.py
@@ -6,30 +6,38 @@ the Debug Adapter Protocol (https://microsoft.github.io/debug-adapter-protocol/)
 
 """
 
-from interface import debugger_queue, start_response_thread, read_debugger_input
 from util import log, json, run_in_new_thread
+from interface import DebuggerInterface
 import time
+
+interface = None
 
 
 def main():
     """
-    First function to be called. Launches necessary threads to read/write to stdin/stdout
+    First function to be called. Creates a DebuggerInterface instance and starts it
     """
 
+    global interface
     log("--- WARNING --- : Adapter not set up")
 
-    # Call this function at the start of main. It starts the thread that will send queued 
-    # messages to the debugger through stdout
-    start_response_thread()
+    # Create a DebuggerInterface instance in main, optionally passing a function 
+    # to call when a message is recieved from the debugger
+    interface = DebuggerInterface(on_recieve=on_recieve)
 
-    # Call this function at the end of main, with your callback function as the argument
-    read_debugger_input(callback)
+    # Now all you have to do is:
+    interface.start()
+
+    # And if you want an unblocking statement:
+    # interface.start_nonblocking()
+    # 
+    # But make sure main remains blocked, otherwise the adapter will stop.
 
 
-def callback(message: str):
+def on_recieve(message: str):
     """
     Gets called every time a new message is received from the debugger.
-    The callback is run in the same thread as the recieving loop, so make sure it is non-blocking.
+    This function is run in the same thread as the message-recieving loop, so make sure it is non-blocking.
     """
 
     # All dap messages are in json format, so you will probably want to convert them 
@@ -40,7 +48,7 @@ def callback(message: str):
     # in the debugger queue in their string/dumped form
     tmp = {'request_seq': dap_msg['seq']}
     res_msg = json.dumps(tmp)
-    debugger_queue.put(res_msg)
+    interface.send(res_msg)
 
     # If you need to do heavier processing, need blocking statements, 
     # or want controlled infinite loops, run an external function in a new thread.
--- adapter/interface.py
+++ adapter/interface.py
@@ -4,68 +4,87 @@ from queue import Queue
 from util import CONTENT_HEADER, run_in_new_thread, log
 
 
-debugger_queue = Queue()
-
-
-def read_debugger_input(callback: function):
+class DebuggerInterface:
     """
-    Reads DAP messages sent from the debugger through stdin and calls the
-    function passed in as the callback with the message recieved.
+    Provides a simple interface to capture and send 
+    messages from/to the debugger vis stdin/stdout.
     """
 
-    while True:
-        try:
-            content_length = 0
-            while True:
-                header = stdin.readline()
-                if header:
-                    header = header.strip()
-                if not header:
-                    break
-                if header.startswith(CONTENT_HEADER):
-                    content_length = int(header[len(CONTENT_HEADER):])
+    def __init__(self, on_recieve: function = None):
+        self.send_queue = Queue()
+        self.running = False
+        self.callback = on_recieve
 
-            if content_length > 0:
-                total_content = ""
-                while content_length > 0:
-                    content = stdin.read(content_length)
-                    content_length -= len(content)
-                    total_content += content
+    def start(self):
+        if not self.running:
+            self.running = True
+            run_in_new_thread(self._debugger_send_loop)
+            self._read_debugger_input()
+    
+    def start_nonblocking(self):
+        if not self.running:
+            self.running = True
+            run_in_new_thread(self._debugger_send_loop)
+            run_in_new_thread(self._read_debugger_input)
 
-                if content_length == 0:
-                    message = total_content
-                    callback(message)
+    def stop(self):
+        if self.running:
+            self.running = False
 
-        except Exception as e:
-            log("Failure reading stdin: " + str(e))
-            return
+    def send(self, message: str):
+        self.send_queue.put(message)
 
+    def _read_debugger_input(self, callback: function):
+        """
+        Reads DAP messages sent from the debugger through stdin and calls the
+        function passed in as the callback with the message recieved.
+        """
 
-def _debugger_send_loop():
-    """
-    Waits for items to show in the send queue and prints them.
-    Blocks until an item is present
-    """
-
-    while True:
-        msg: str = debugger_queue.get()
-        if msg is None:
-            return
-        else:
+        while self.running:
             try:
-                stdout.write('Content-Length: {}\r\n\r\n'.format(len(msg)))
-                stdout.write(msg)
-                stdout.flush()
-                log('Sent to Debugger:', msg)
+                content_length = 0
+                while self.running:
+                    header = stdin.readline()
+                    if header:
+                        header = header.strip()
+                    if not header:
+                        break
+                    if header.startswith(CONTENT_HEADER):
+                        content_length = int(header[len(CONTENT_HEADER):])
+
+                if content_length > 0:
+                    total_content = ""
+                    while content_length > 0:
+                        content = stdin.read(content_length)
+                        content_length -= len(content)
+                        total_content += content
+
+                    if content_length == 0:
+                        message = total_content
+                        if self.callback:
+                            self.callback(message)
+
             except Exception as e:
-                log("Failure writing to stdout (normal on exit):" + str(e))
+                log("Failure reading stdin: " + str(e))
                 return
 
 
-def start_response_thread():
-    """
-    Simple wrapper to start the debugger send loop below in a new thread
-    """
+    def _debugger_send_loop(self):
+        """
+        Waits for items to show in the send queue and prints them.
+        Blocks until an item is present
+        """
 
-    run_in_new_thread(_debugger_send_loop)
+        while self.running:
+            msg: str = self.send_queue.get()
+            if msg is None:
+                return
+            else:
+                try:
+                    stdout.write('Content-Length: {}\r\n\r\n'.format(len(msg)))
+                    stdout.write(msg)
+                    stdout.flush()
+                    log('Sent to Debugger:', msg)
+                except Exception as e:
+                    log("Failure writing to stdout (normal on exit):" + str(e))
 
